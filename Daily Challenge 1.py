578c419510890f81da2bf2d617966b012edce615
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-07-20 17:54:14 +0100

    Removed old file.

--- Daily Challenge 1.py
+++
@@ -1,5 +0,0 @@
-DevOps = ["Clifford","Christopher","John","Jason","Arsalan","Domenico","Joshua","Ryan","Bradley","Javas","Jacob","Sithembiso","Wasim","Mohamed","Tobias","Christopher","Samuel","Edmund","Amanda","Steven","Jack"]
-DevOps.append("Luke")
-print (DevOps)
-print (DevOps[4])
-print (DevOps.count("Christopher"))
